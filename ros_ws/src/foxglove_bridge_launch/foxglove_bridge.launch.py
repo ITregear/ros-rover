b5d4ebb163b92c6266c4d6d1265fdc7473985af0
Ivan Tregear <ivantregear@gmail.com>
2025-06-07 13:09:04 +0100

    foxglove bridge

---
+++ ros_ws/src/foxglove_bridge_launch/foxglove_bridge.launch.py
@@ -0,0 +1,15 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='foxglove_bridge',
+            executable='foxglove_bridge',
+            name='foxglove_bridge',
+            output='screen',
+            parameters=[{
+                "port": 8765  # Default port, can be changed if needed
+            }]
+        )
+    ])
